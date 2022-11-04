432261054c194917ba9e226c6eb9439db599932f
Eduardo Lago <laguillo@gmail.com>
2022-11-03 19:06:15 -0500

    Fix Update resultado

--- Controladores/ControladorResultado.py
+++ Controladores/ControladorResultado.py
@@ -6,13 +6,14 @@ from Repositorio.RepositorioMesa import RepositorioMesa
 from Repositorio.RepositorioResultado import RepositorioResultado
 from bson import ObjectId
 
+
 class ControladorResultado():
     def __init__(self):
         self.repositorioResultado = RepositorioResultado()
         self.repositorioCandidato = RepositorioCandidato()
         self.repositorioMesa = RepositorioMesa()
 
-    def createResultado(self,infoResultado,idM,idC):
+    def createResultado(self, infoResultado, idM, idC):
         elresultado = ModeloResultado(infoResultado)
         lamesa = ModeloMesa(self.repositorioMesa.findById(idM))
         elcandidato = ModeloCandidato(self.repositorioCandidato.findById(idC))
@@ -27,18 +28,17 @@ class ControladorResultado():
     def showallResultado(self):
         return self.repositorioResultado.findAll()
 
-    def updateResultado(self, infoResultado,idM,idC):
-        resultadoactual = ModeloResultado(self.repositorioResultado.findById(infoResultado))
-        resultadoactual.numero_votos = infoResultado["numero_votos"]
-        lamesa=ModeloMesa(self.repositorioMesa.findById(idM))
-        elcandido=ModeloCandidato(self.repositorioCandidato.findById(idC))
+    def updateResultado(self, idR, idM, idC, infoResultado):
+        resultadoactual = ModeloResultado(self.repositorioResultado.findById(idR))
+        lamesa = ModeloMesa(self.repositorioMesa.findById(idM))
+        elcandido = ModeloCandidato(self.repositorioCandidato.findById(idC))
         resultadoactual.mesa = lamesa
         resultadoactual.candidato = elcandido
+        resultadoactual.numero_votos = infoResultado["numero_votos"]
         return self.repositorioResultado.save(resultadoactual)
 
     def deleteResultado(self, id):
-        self.repositorioResultado.delete(id)
-        return True
+        return self.repositorioResultado.delete(id)
 
-    def listarresultadoscandidato(self,idCa):
+    def listarresultadoscandidato(self, idCa):
         return self.repositorioResultado.getlistResultadosCandidato(idCa)
